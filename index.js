c7b5c6354c070a190d5057084a67966787550d93
Md. Mehedi Hasan <126223424+huzzathasan@users.noreply.github.com>
2024-06-08 19:51:21 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// Define the schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Define the resolvers
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+// Create the Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Start the server
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
